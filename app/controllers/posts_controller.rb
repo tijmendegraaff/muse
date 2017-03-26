efb1744964d82fbf5b02a6572ba4340198692a06
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-26 01:28:19 +0100

    added views for post new and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,43 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :link, :description)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 end
