00d6d1388158f002053cb8bd105749ae8346e681
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-26 13:53:45 +0200

    added new comment model and associated views

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = @post.id
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  # def create
+  #   @post = Post.find(params[:post_id])
+  #   @comment = @post.comments.build(comment_params)
+
+  # end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
