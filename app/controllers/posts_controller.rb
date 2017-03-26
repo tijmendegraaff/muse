32d9dcc7231e130e4f864a2a3c122f0a65e4a854
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-26 01:57:36 +0100

    added views for crud Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
@@ -25,9 +25,16 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
