ec41c1954898e7fa7513457ea95019807ea9ca36
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-26 12:53:44 +0200

    image upload added to model and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :link, :description)
+    params.require(:post).permit(:title, :link, :description, :photo, :photo_cache)
   end
 
   def find_post
---
+++ db/migrate/20170326103140_add_photo_to_post.rb
@@ -0,0 +1,5 @@
+class AddPhotoToPost < ActiveRecord::Migration[5.0]
+  def change
+    add_column :posts, :photo, :string
+  end
+end
